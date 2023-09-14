8e67da75c0cf948e3d0418232c497cafdb388a0a
kritikaaryal <karyal24@groton.org>
2023-09-14 11:34:59 -0400

    first checkin

---
+++ first program.py
@@ -0,0 +1,7 @@
+from stanfordkarel import *
+
+def main():
+    """ code goes here"""
+    
+if __name__ == "_main_":
+    run_karel_program()
---
+++ firstprogram.py
@@ -0,0 +1,15 @@
+from stanfordkarel import *
+
+def main():
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    move()
+    
+if __name__ == "__main__":
+    run_karel_program()
