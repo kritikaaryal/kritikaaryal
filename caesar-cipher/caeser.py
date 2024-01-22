2a421741b364c4193ef75f109a4caf70599e9765
kritikaaryal <karyal24@groton.org>
2024-01-22 13:50:59 -0500

    Update caeser.py

--- caesar-cipher/caeser.py
+++ caesar-cipher/caeser.py
@@ -23,10 +23,21 @@ def main():
         for char in line:
     result += encrypt_letter(char,shift)
         return result
-    index = ord(char)
-    shifted_index = index + key
-        result = chr(shifted_index)
-    return result
-
+    return chr(ord(char) + key)
+    OUT_OF_BOUNDS = 127
+    FIRST_VISIBLE = 32
+    RANGE = OUT_OF_BOUNDS - FIRST_VISIBLE # This evaluates to 95
+    if shifted_index >= OUT_OF_BOUNDS :
+        shifted_index = shifted_index - OUT_OF_BOUNDS + FIRST_VISIBLE
+    return chr((ord(char) - FIRST_VISIBLE + KEY ) % RANGE + FIRST_VISIBLE)
+def encrypt_line(line:str, key:str, mode="encrypt") -> str:
+    sign = 1
+    if mode.lower() == "decrypt" or mode.lower() == "d":
+        sign = -1
+    shift = (ord(key[0]) - 32) * sign
+    result = ""
+    for char in line:
+    result += encrypt_letter(char,shift)
+return result
 if __name__ == "__main__":
     main()
