e5423ff542acc9b523aca55e93461a830271a69c
kritikaaryal <karyal24@groton.org>
2024-01-23 21:08:30 -0500

    Update caeser.py

--- caesar-cipher/caeser.py
+++ caesar-cipher/caeser.py
@@ -1,18 +1,46 @@
 import sys
-def main():
-    for line in sys.stdin:
-        line = line.strip()
-        print(line)
-        result = ""
-        for line in sys.stdin:
-            line = line.strip()
-            result += encrypt_line(line) + "\n"
-    print(result)
-def encrypt_line(line:str) -> str:
+OUT_OF_BOUNDS = 127
+FIRST_VISIBLE = 32
+RANGE = OUT_OF_BOUNDS - FIRST_VISIBLE # This evaluates to 95
+if shifted_index >= OUT_OF_BOUNDS :
+    shifted_index = shifted_index - OUT_OF_BOUNDS + FIRST_VISIBLE
+def encrypt_letter(char:str, key:str) -> str:
+# We will figure out how to do the encryption later.
+# For now, we're just going to return the letter "x"
+    return chr(ord(char) + key)
+
+def encrypt_line(line:str, key:str, mode="encrypt") -> str:
+    sign = 1
+    if mode.lower() == "decrypt" or mode.lower() == "d":
+        sign = -1
+    shift = (ord(key[0]) - 32) * sign
     result = ""
     for char in line:
-        result += encrypt_letter(char)
-return result
+        result += encrypt_letter(char,shift)
+    return result
+def encrypt_message(key:str, mode="encrypt") -> None:
+    result = ""
+    for line in sys.stdin:
+        line = line.strip()
+        result += encrypt_line(line, key, mode) + "\n"
+    print(result)
+def main_menu() -> str:
+    print("[1] Enter Encryption Key")
+    print("[2] Encrypt a Message")
+    print("[3] Decrypt a Message")
+    print("[0] Exit Program")
+# We call strip after calling input to get rid of any
+# leading or trailing white space entered by the user
+    return input("Please select an option: ").strip()
+def main():
+    key = input("Please enter the encryption key: ")
+    result = ""
+    for line in sys.stdin:
+        line = line.strip()
+        result += encrypt_line(line, key) + "\n"
+    print(result)
+    
+
 if __name__ == "__main__":
     main()
 
